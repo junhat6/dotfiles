fd227b3988298c2d04b07cb415a3a8fc9d6f4c7d
junichihattori <junhat6@gmail.com>
2025-09-26 00:32:55 +0900

    keymaps二コメントを追加

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,10 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>j", vim.cmd.Ex)
 vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
+
+-- :q :wなどのコマンド系
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close window" })
 vim.keymap.set("n", "<leader>Q", "<cmd>qa<CR>", { desc = "Quit all" })
 vim.keymap.set("n", "<leader>o", "<cmd>only<CR>", { desc = "Close other windows" })
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+
+-- ウィンドウ移動
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
