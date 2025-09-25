db07d01f6b10f3600ef711ebf42fcbbf4ae22662
junichihattori <junhat6@gmail.com>
2025-09-26 00:24:50 +0900

    :q :wコマンドのショートカットをマッピング

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>j", vim.cmd.Ex)
 vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close window" })
+vim.keymap.set("n", "<leader>Q", "<cmd>qa<CR>", { desc = "Quit all" })
+vim.keymap.set("n", "<leader>o", "<cmd>only<CR>", { desc = "Close other windows" })
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
