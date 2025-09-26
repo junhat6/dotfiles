1e076c518b73ca71a7defdcf59e6c4491c0a9f9c
junichihattori <junhat6@gmail.com>
2025-09-26 22:10:14 +0900

    blinkの導入

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -27,6 +27,10 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
+	{ src = "https://github.com/Saghen/blink.cmp" },
+	-- スニペット
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/rafamadriz/friendly-snippets" },
 
 	-- ── Formatting ────────────────────────────────────────────
 	{ src = "https://github.com/stevearc/conform.nvim" },
